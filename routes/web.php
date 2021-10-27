ec6848b74ca4c590d10f18dda4db4e624f794ff3
Kulthe Snehal <snehal.kulthe.1@gmail.com>
2021-10-27 12:05:40 +0530

    added date filter

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,4 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::post('/submit', [App\Http\Controllers\PaymentController::class, 'save'])->name('submit');
 Route::get('/home', [App\Http\Controllers\PaymentController::class, 'show'])->name('list');
+Route::get('/datatable', [App\Http\Controllers\HomeController::class, 'index'])->name('datatable');
